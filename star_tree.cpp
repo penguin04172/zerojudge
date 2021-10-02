e5bdbdc0e31bc3437d901562b343aa6fc83e99ee
zxsaqw21123 <b10800002@gemail.yuntech.edu.tw>
2021-10-02 22:03:20 +0800

    20211002_StarTree

--- star_tree.cpp
+++ star_tree.cpp
@@ -5,17 +5,25 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	int x[10];
-	for (int i=0 ; i<n ; i++) {
-		cin >> x[i];
-	}
 	
-	int temp;
-//	x[0] <-> x[n-1]
-	temp = x[0];
-	x[0] = x[n-1];
-	x[n-1] = temp;
+//	n列
+	for (int i=0 ; i<n ; i++) {
+//		每列要做?
+		for (int j=0 ; j<n-i-1 ; j++) {
+//			推空格 
+			cout << ' ';
+		}
+		for (int j=0 ; j<i*2+1 ; j++) {
+//			畫星星 
+			cout << '*';
+		}
+		cout << '\n'; 
+	}
 	
 	return 0;
 }
+//n=3
+//  *	i=0
+// ***	i=1
+//*****	i=2
 
